16b7b0ee5e48c71dfd85ce68ea64e7fdcdfab614
cyberflower <steve1401@korea.ac.kr>
2019-12-29 22:20:50 +0900

    Add Codeforces tag

--- auto-edit.py
+++ auto-edit.py
@@ -25,11 +25,25 @@ def change_all_file(files,ori,new,cnt=0):
             read_file=tmp.sub(new,read_file,cnt)
             write_file=open(file,'w',encoding='UTF-8', errors='ignore')
             write_file.write(read_file) 
-            print(file)           
+            print(file)       
+
+# change files which consists specific substr in filenames
+# change every word in contents
+# if cnt given, change word less than cnt times per files
+def change_spec_file(files,sub_str,ori,new,cnt=0):
+    for file in files:
+        if os.path.isfile(file) and sub_str in file:
+            open_file=open(file,'r',encoding='UTF-8', errors='ignore')
+            read_file=open_file.read()
+            tmp=re.compile(ori)
+            read_file=tmp.sub(new,read_file,cnt)
+            write_file=open(file,'w',encoding='UTF-8', errors='ignore')
+            write_file.write(read_file) 
+            print(file)    
 
 if __name__=="__main__":
     os.chdir(os.getcwd()+"/_posts")
     #os.chdir(os.getcwd())
     files=glob.glob('*')
     #change_filename(files,"2","a")
-    change_all_file(files,'SKH-camp','PS',cnt=2)
+    change_spec_file(files,'cf','PS','Codeforces',cnt=2)
